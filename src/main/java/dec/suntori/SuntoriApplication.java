e1fd113d35c3dcc237cf8a4b0a8d77ff94902123
hajin <kimhajin01@naver.com>
2024-12-13 19:43:54 +0900

    Feat: Create new project

---
+++ src/main/java/dec/suntori/SuntoriApplication.java
@@ -0,0 +1,13 @@
+package dec.suntori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuntoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuntoriApplication.class, args);
+	}
+
+}
