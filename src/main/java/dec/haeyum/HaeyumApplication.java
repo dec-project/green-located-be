e7307385afcc4ba2f9f68af148278266121406c7
hajin <kimhajin01@naver.com>
2024-12-22 17:56:14 +0900

    create new project

---
+++ src/main/java/dec/haeyum/HaeyumApplication.java
@@ -0,0 +1,13 @@
+package dec.haeyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeyumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dec/haeyum/HaeyumApplicationTests.java
@@ -0,0 +1,13 @@
+package dec.haeyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeyumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
