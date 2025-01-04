f1c1ba1b15ea5c4eb8ac49de24f9691123ced0a0
KoreaBean <ksg04001@daum.net>
2025-01-05 04:23:19 +0900

    날씨 API - 날씨 별 이미지 삽입 시 기존 이미지 삭제 로직 추가

--- src/main/java/dec/haeyum/config/error/ErrorCode.java
+++ src/main/java/dec/haeyum/config/error/ErrorCode.java
@@ -15,8 +15,9 @@ public enum ErrorCode {
     NOT_EXISTED_DATETIME_PARSE(HttpStatus.BAD_REQUEST,"ED","유효하지 않은 날짜 형식입니다. ex)2024-12-25"),
     NOT_EXISTED_CALENDAR(HttpStatus.NOT_FOUND,"EC","찾을 수 있는 달력이 없습니다."),
     // 날씨 에러
+    CANT_DELETE_FILE(HttpStatus.INTERNAL_SERVER_ERROR,"CD","삭제할 이미지 파일을 찾지 못했습니다."),
     NOT_EXISTED_WEATHERIMG(HttpStatus.NOT_FOUND,"EI","존재하지 않는 날씨 이미지입니다."),
-    // 이미지 전송 에러
+    // 이미지 에러
     NOT_EXISTED_IMG(HttpStatus.INTERNAL_SERVER_ERROR,"EI","존재하지 않는 이미지 파일입니다.");
 
     private final String message;
