248521bbb33671edcc9579231649dcb14ae6e2bf
hajin <kimhajin01@naver.com>
2025-01-06 19:22:35 +0900

    Feat: Add calendar song data
    2025.01.06
    calendarSong 데이터 삽입 배치 로직 추가
    
    Related to: #5

--- src/main/java/dec/haeyum/song/config/CalendarSongCsvScheduleWriter.java
+++ src/main/java/dec/haeyum/song/config/CalendarSongCsvScheduleWriter.java
@@ -1,9 +1,38 @@
 package dec.haeyum.song.config;
 
+import dec.haeyum.calendar.repository.CalendarRepository;
+import dec.haeyum.song.dto.CalendarSongCsvDto;
+import dec.haeyum.song.entity.CalendarSong;
+import dec.haeyum.song.entity.Song;
+import dec.haeyum.song.entity.TemporaryCalendarSong;
+import dec.haeyum.song.repository.CalendarSongRepository;
+import dec.haeyum.song.repository.SongRepository;
+import dec.haeyum.song.repository.TemporaryCalendarSongRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
-public class CalendarSongCsvScheduleWriter {
+public class CalendarSongCsvScheduleWriter implements ItemWriter<CalendarSongCsvDto> {
+
+    private final TemporaryCalendarSongRepository temporaryCalendarSongRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public void write(Chunk<? extends CalendarSongCsvDto> chunk) throws Exception {
+        Chunk<TemporaryCalendarSong> temporaryCalendarSongs = new Chunk<>();
+
+        chunk.forEach(CalendarSongCsvDto -> {
+            TemporaryCalendarSong temporaryCalendarSong = TemporaryCalendarSong.of(CalendarSongCsvDto);
+            temporaryCalendarSongs.add(temporaryCalendarSong);
+        });
+        temporaryCalendarSongRepository.saveAll(temporaryCalendarSongs);
+    }
+
+
+
+
 }
--- src/main/java/dec/haeyum/song/config/SimpleJobConfiguration.java
+++ src/main/java/dec/haeyum/song/config/SimpleJobConfiguration.java
@@ -1,12 +1,17 @@
 package dec.haeyum.song.config;
 
+import dec.haeyum.song.dto.CalendarSongCsvDto;
 import dec.haeyum.song.dto.SongDetailCsvDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -18,18 +23,58 @@ public class SimpleJobConfiguration {
     private final SongDetailCsvReader songDetailCsvReader;
     private final SongDetailCsvScheduleWriter songDetailCsvScheduleWriter;
 
+    private final CalendarSongCsvReader calendarSongCsvReader;
+    private final CalendarSongCsvScheduleWriter calendarSongCsvScheduleWriter;
+
     @Bean
-    public Job SongDetailDataLoadJob(JobRepository jobRepository, Step songDetailDataLoadJobStep) {
-        return new JobBuilder("SongDetailDataLoadJob", jobRepository)
-                .start(songDetailDataLoadJobStep).build();
+    public Job sequentialJob(JobRepository jobRepository, Flow jobFlow) {
+        // Flow를 Job으로 연결
+        return new JobBuilder("SequentialJob", jobRepository)
+                .start(jobFlow) // Flow 실행
+                .end()          // Job 종료
+                .build();
     }
 
+    @Bean
+    public Flow jobFlow(@Qualifier("SongDetailDataLoadStep") Step songDetailDataLoadStep, @Qualifier("CalendarSongDataLoadStep") Step calendarSongDataLoadStep) {
+        // Flow 내부에서 Step 순서 정의
+        return new FlowBuilder<SimpleFlow>("JobFlow")
+                .start(songDetailDataLoadStep)      // 첫 번째 Step
+                .next(calendarSongDataLoadStep)    // 두 번째 Step
+                .build();
+    }
+/*
+    @Bean
+    public Job SongDetailDataLoadJob(JobRepository jobRepository, @Qualifier("SongDetailDataLoadStep") Step songDetailDataLoadStep) {
+        return new JobBuilder("SongDetailDataLoadJob", jobRepository)
+                .start(songDetailDataLoadStep).build();
+    }
+
+
+ */
     @Bean
     public Step SongDetailDataLoadStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("SongDetailDataLoadStep", jobRepository)
                 .<SongDetailCsvDto, SongDetailCsvDto>chunk(100, platformTransactionManager)
                 .reader(songDetailCsvReader.csvScheduleReader())
                 .writer(songDetailCsvScheduleWriter)
+                .build();
+    }
+/*
+    @Bean
+    public Job CalendarSongDataLoadJob(JobRepository jobRepository, @Qualifier("CalendarSongDataLoadStep") Step calendarSongDataLoadStep) {
+        return new JobBuilder("CalendarSongDataLoadJob", jobRepository)
+                .start(calendarSongDataLoadStep).build();
+    }
+
+
+ */
+    @Bean
+    public Step CalendarSongDataLoadStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("CalendarSongDataLoadStep", jobRepository)
+                .<CalendarSongCsvDto, CalendarSongCsvDto>chunk(100, platformTransactionManager)
+                .reader(calendarSongCsvReader.csvScheduleReader())
+                .writer(calendarSongCsvScheduleWriter)
                 .build();
     }
 
