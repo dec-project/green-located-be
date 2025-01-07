57cb5de2919ddba78af50e4ecc45673eb5a16009
KoreaBean <ksg04001@daum.net>
2025-01-08 01:08:29 +0900

    imgUrl http 경로 삭제

--- src/main/java/dec/haeyum/movie/dto/response/MovieItem.java
+++ src/main/java/dec/haeyum/movie/dto/response/MovieItem.java
@@ -14,20 +14,19 @@ public class MovieItem {
     private String title;
     private String img;
 
-    protected MovieItem(Long movieId, Integer ranking, String title, String img, String file_url) {
+    protected MovieItem(Long movieId, Integer ranking, String title, String img) {
         this.movieId = movieId;
         this.ranking = ranking;
         this.title = title;
-        this.img = file_url+img;
+        this.img = "image/" + img;
     }
 
-    public static List<MovieItem> asList(List<Top5MoviesDto> list, String file_url) {
+    public static List<MovieItem> asList(List<Top5MoviesDto> list) {
         return list.stream()
                 .map(dto -> new MovieItem(dto.getMovieId(),
                         dto.getRanking(),
                         dto.getTitle(),
-                        dto.getImg(),
-                        file_url))
+                        dto.getImg()))
                 .collect(Collectors.toList());
     }
 }
