6a9be17a19c3b5c59b8dcc53abc530606fb33c39
KoreaBean <ksg04001@daum.net>
2025-02-02 01:09:19 +0900

    조회수 트랜잭션 삭제

--- src/main/java/dec/haeyum/calendar/service/impl/CalendarServiceImpl.java
+++ src/main/java/dec/haeyum/calendar/service/impl/CalendarServiceImpl.java
@@ -113,12 +113,10 @@ public  class CalendarServiceImpl implements CalendarService {
     }
 
     @Override
-    @Transactional
     public void increaseViewCount(CalendarEntity calendar) {
         log.info("increaseViewCount before={}",calendar.getViewCount());
         calendar.increaseViewCount();
         calendarRepository.save(calendar);
-        calendarRepository.flush();
         log.info("increaseViewCount after ={}",calendar.getViewCount());
     }
 
